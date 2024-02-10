ae0a92e4e8e505ec3f3031fd2d092c1da3f4b13b
Adi Dahari <dahariadi16@gmail.com>
2024-02-10 15:37:06 +0200

    partial views and basic functions

--- helpers.php
+++ helpers.php
@@ -10,3 +10,65 @@ function basePath($path = '')
 {
   return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a partial view
+ *
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name)
+{
+  $partialPath = basePath("views/partials/{$name}.php");
+
+  inspect($partialPath);
+
+
+  if (!file_exists($partialPath)) {
+    throw new Exception("Partial `{$name}` not found");
+  }
+  require $partialPath;
+}
+
+/**
+ * Load a view
+ *
+ * @param string $name
+ * @return void
+ */
+function loadView($name)
+{
+  $viewPath = basePath("views/{$name}.view.php");
+
+  if (!file_exists($viewPath)) {
+    throw new Exception("View `{$name}` not found");
+  }
+  require $viewPath;
+}
+
+/**
+ * Inspect value(s)
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect value(s) and die 
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+  die();
+}
