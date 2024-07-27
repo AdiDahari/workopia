aa626747ae7860b59f4459e98a1cad62d6a705e4
adi <dahariadi16@gmail.com>
2024-07-27 17:31:55 +0300

    layout partials separation

--- helpers.php
+++ helpers.php
@@ -10,3 +10,64 @@ function basePath($path = "")
 {
   return __DIR__ . "/" . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name)
+{
+  $viewPath = basePath("views/{$name}.view.php");
+
+  if (file_exists($viewPath)) {
+    require $viewPath;
+  } else {
+    echo "View '{$name}' does not exists";
+  }
+}
+
+/**
+ * Load a partial
+ *
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name)
+{
+  $partialPath = basePath("views/partials/{$name}.partial.php");
+
+  if (file_exists($partialPath)) {
+    require $partialPath;
+  } else {
+    echo "Partial '{$name}' does not exists";
+  }
+}
+
+/**
+ * Inspect value(s)
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo "<pre>";
+  var_dump($value);
+  echo "</pre>";
+}
+
+/**
+ * Inspect value(s) and die
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo "<pre>";
+  var_dump($value);
+  echo "</pre>";
+  die();
+}
